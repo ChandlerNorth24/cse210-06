f491dd6fbd47195a1ef440ff717e9805e92ca28c
KillerK1998 <kaler.sappington@gmail.com>
2022-12-06 19:25:20 -0700

    added changes to basic_item and legendary_item

---
+++ basic_item.py
@@ -0,0 +1,6 @@
+class basic_item:
+
+    def __init__(self):
+
+        super(basic_item, self).__init__()
+        self.appearance = "$"
---
+++ legendary_item.py
@@ -0,0 +1,6 @@
+class legendary_item:
+
+    def __init__(self):
+
+        super(legendary_item, self).__init__()
+        self.appearance = "$"
