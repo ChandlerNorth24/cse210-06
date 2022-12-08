8313034e33ba92d6f46359cea25aee92fe7a98a9
Ejpulver <pul15004@byui.edu>
2022-12-07 17:16:28 -0700

    added shared method in basic_item class

--- basic_item.py
+++ basic_item.py
@@ -1,6 +1,11 @@
-class basic_item:
+from object_found import object_found
+class basic_item(object_found):
 
     def __init__(self):
 
         super(basic_item, self).__init__()
         self.appearance = "$"
+    
+    def found_object(self):
+        pass
+    
