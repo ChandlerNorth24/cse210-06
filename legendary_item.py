5e4c843ffe20c37528254a3846ba21405b530414
Ejpulver <pul15004@byui.edu>
2022-12-07 17:15:41 -0700

    changes to legendary item

--- legendary_item.py
+++ legendary_item.py
@@ -1,6 +1,11 @@
-class legendary_item:
+from object_found import object_found
+class legendary_item(object_found):
 
     def __init__(self):
 
         super(legendary_item, self).__init__()
         self.appearance = "$"
+
+    def found_object(self):
+        # write code here for legendary object
+        pass
